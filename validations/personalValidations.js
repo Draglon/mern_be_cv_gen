68150d73e6c45ca80c61df0a109321fea1dcdf43
Draglon <s.nazarchuck@gmail.com>
2026-04-16 20:01:43 +0300

    Fix validations

--- validations/personalValidations.js
+++ validations/personalValidations.js
@@ -1,64 +1,74 @@
 import { body } from 'express-validator'
 
+import { MIN_INPUT_LENGTH, MAX_INPUT_LENGTH, MAX_EMAIL_LENGTH, MAX_PHONE_NUMBER_LENGTH, MAX_TEXTAREA_CONTENT_NORMAL_LENGTH } from "../lib/constants/index.js";
+
 export const personalInfoValidation = [
-  body('sectionTitle', 'Укажите название раздела.').optional(),
+  body('sectionTitle', 'Укажите название раздела.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }).optional(),
   body('userUrl', 'Неверная ссылка на фотографию.').optional(),
-  body('firstName', 'Имя должно быть минимум 3 символа.').isLength({ min: 3 }),
-  body('lastName', 'Фамилия должна быть минимум 3 символа.').isLength({ min: 3 }),
-  body('email', 'Неверный формат почты.').trim().isEmail(),
-  body('address', 'Укажите адрес.').optional(),
-  body('phoneNumber', 'Укажите номер телефона.').optional(),
-  body('birthday', 'Укажите дату рождения.').optional(),
-  body('linkedIn', 'Укажите linkedIn.').optional(),
-]
+  body('firstName', 'Имя должно быть минимум 3 символа.').isLength({ min: MIN_INPUT_LENGTH }),
+  body('lastName', 'Фамилия должна быть минимум 3 символа.').isLength({ min: MIN_INPUT_LENGTH }),
+  body('email', 'Неверный формат почты.').trim().isEmail().isLength({ max: MAX_EMAIL_LENGTH }),
+  body('address', 'Укажите адрес.').isLength({ max: MAX_INPUT_LENGTH }),
+  body('phoneNumber', 'Укажите номер телефона.').isLength({ max: MAX_PHONE_NUMBER_LENGTH }),
+  body('birthday', 'Укажите дату рождения.'),
+  body('linkedIn', 'Укажите linkedIn.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }).optional(),
+  body('telegram', 'Укажите telegram.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }).optional(),
+  body('portfolio', 'Укажите portfolio.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }).optional(),
+];
 
 export const personalHobbiesValidation = [
-  body('hobbies', 'Укажите хобби или интерес.').optional(),
-]
+  body('sectionTitle', 'Укажите название раздела.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }).optional(),
+  body('hobbies', 'Укажите хобби или интерес.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+];
 
 export const personalLanguagesValidation = [
-  body('language', 'Укажите язык.').optional(),
-  body('level', 'Укажите уровень знания языка.').optional(),
-]
+  body('sectionTitle', 'Укажите название раздела.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }).optional(),
+  body('language', 'Укажите язык.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('level', 'Укажите уровень знания языка.'),
+];
 
 export const personalExperienceValidation = [
-  body('sectionTitle', 'Укажите название раздела.').optional(),
+  body('sectionTitle', 'Укажите название раздела.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }).optional(),
   body('lastPlacesOfWorks', 'Укажите число последних мест работ.').optional(),
-  body('position', 'Укажите должность.').optional(),
-  body('companyName', 'Укажите название компании.').optional(),
-  body('location', 'Укажите место работы.').optional(),
-  body('placeOfWork', 'Укажите тип работы.').optional(),
-  body('workingTime', 'Укажите рабочее время.').optional(),
-  body('startDate', 'Укажите время начала работы.').optional(),
-  body('endDate', 'Укажите время окончания работы.').optional(),
-  body('description', 'Укажите описание своей работы.').optional(),
+  body('position', 'Укажите должность.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('companyName', 'Укажите название компании.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('location', 'Укажите место работы.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('placeOfWork', 'Укажите тип работы.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('workingTime', 'Укажите рабочее время.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('startDate', 'Укажите время начала работы.'),
+  body('endDate', 'Укажите время окончания работы.'),
+  body('description', 'Укажите описание своей работы.').isLength({ max: MAX_TEXTAREA_CONTENT_NORMAL_LENGTH }),
   body('skills', 'Укажите используемые навыки.').optional(),
-]
+];
 
 export const personalEducationValidation = [
-  body('institute', 'Укажите институт.').optional(),
-  body('degree', 'Укажите степень образования.').optional(),
-  body('faculty', 'Укажите факультет.').optional(),
-  body('specialization', 'Укажите специализацию.').optional(),
-  body('startDate', 'Укажите дату начала обучения.').optional(),
-  body('endDate', 'Укажите дату конеца обучения.').optional(),
-]
+  body('sectionTitle', 'Укажите название раздела.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }).optional(),
+  body('institute', 'Укажите институт.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('degree', 'Укажите степень образования.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('faculty', 'Укажите факультет.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('specialization', 'Укажите специализацию.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('startDate', 'Укажите дату начала обучения.'),
+  body('endDate', 'Укажите дату конеца обучения.'),
+];
 
 export const personalCoursesValidation = [
-  body('course', 'Укажите название курса.').optional(),
-  body('description', 'Укажите описание курса.').optional(),
-  body('startDate', 'Укажите дату начала обучения.').optional(),
-  body('endDate', 'Укажите дату конеца обучения.').optional(),
-]
+  body('sectionTitle', 'Укажите название раздела.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }).optional(),
+  body('course', 'Укажите название курса.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('description', 'Укажите описание курса.').isLength({ max: MAX_TEXTAREA_CONTENT_NORMAL_LENGTH }),
+  body('startDate', 'Укажите дату начала обучения.'),
+  body('endDate', 'Укажите дату конеца обучения.'),
+];
 
 export const personalSkillsValidation = [
-  body('skill', 'Укажите название навыка.').optional(),
-  body('level', 'Укажите уровень владения навыком.').optional(),
-  body('visible').optional(),
-]
+  body('sectionTitle', 'Укажите название раздела.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }).optional(),
+  body('skill', 'Укажите название навыка.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('level', 'Укажите уровень владения навыком.'),
+  body('visible'),
+];
 
 export const personalToolsValidation = [
-  body('tool', 'Укажите название инструмента.').optional(),
-  body('level', 'Укажите уровень владения инструментом.').optional(),
-  body('visible').optional(),
-]
+  body('sectionTitle', 'Укажите название раздела.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }).optional(),
+  body('tool', 'Укажите название инструмента.').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+  body('level', 'Укажите уровень владения инструментом.'),
+  body('visible'),
+];
--- validations/userValidations.js
+++ validations/userValidations.js
@@ -1,13 +1,14 @@
-import { body } from 'express-validator'
+import { body } from 'express-validator';
+
+import { MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH, MIN_INPUT_LENGTH, MAX_INPUT_LENGTH, MAX_EMAIL_LENGTH } from "../lib/constants/index.js";
 
 export const loginValidation = [
-  body('email', 'Неверный формат почты').trim().isEmail(),
-  body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
-]
+  body('email', 'Неверный формат почты').trim().isEmail().isLength({ max: MAX_EMAIL_LENGTH }),
+  body('password', 'Пароль должен быть минимум 6 символов').isLength({ min: MIN_PASSWORD_LENGTH, max: MAX_PASSWORD_LENGTH }),
+];
 
 export const registerValidation = [
-  body('email', 'Неверный формат почты').trim().isEmail(),
-  body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5 }),
-  body('userName', 'Имя пользователя должно быть минимум 3 символа').isLength({ min: 3 }),
-  body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
-]
+  body('email', 'Неверный формат почты').trim().isEmail().isLength({ max: MAX_EMAIL_LENGTH }),
+  body('password', 'Пароль должен быть минимум 6 символов').isLength({ min: MIN_PASSWORD_LENGTH, max: MAX_PASSWORD_LENGTH }),
+  body('userName', 'Имя пользователя должно быть минимум 3 символа').isLength({ min: MIN_INPUT_LENGTH, max: MAX_INPUT_LENGTH }),
+];
